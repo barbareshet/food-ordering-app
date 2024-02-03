be3f9d5a26353239990e0d82bfc5c21b951f4aff
ido barnea <barbareshet@gmail.com>
2024-02-03 08:18:06 +0200

    header structure

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
-import { Inter } from "next/font/google";
+import { Roboto } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const roboto = Roboto({ subsets: ["latin"], weight: ["100", "300", "400", "500", "700", "900"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +11,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={roboto.className}>
+      <main className="max-w-4xl mx-auto p-4">
+        {children}
+      </main>
+      </body>
     </html>
   );
 }
